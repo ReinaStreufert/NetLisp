f5523006d3052ee7c2d3e1ea561fc016d72b501f
ReinaStreufert <49079389+ReinaStreufert@users.noreply.github.com>
2025-06-27 00:38:23 -0400

    yayyyy

--- NetLisp/nlshell/Program.cs
+++ NetLisp/nlshell/Program.cs
@@ -2,6 +2,6 @@
 using nlshell;
 
 // delete this forever when anyone uses this
-CopyScripts.Copy(CopyFilter.DllsOnly);
+CopyScripts.Copy(CopyFilter.ScriptsOnly);
 //NLShellContext ctx = new NLShellContext();
 //ctx.Start("wake.nlisp");
